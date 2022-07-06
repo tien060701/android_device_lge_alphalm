1b21aa30089aa821adae742da17ee491916e892c
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-07-06 10:13:03 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_alphaplus.mk
+    $(LOCAL_DIR)/aosp_alphalm.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_alphaplus-userdebug \
-    aosp_alphaplus-eng \
-    aosp_alphaplus-user
+    aosp_alphalm-userdebug \
+    aosp_alphalm-eng \
+    aosp_alphalm-user
