4dee8602db8226ef2f6b6e63d5c7f944f4ff6640
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-07-06 10:11:26 +0700

    Update and rename aosp_alphaplus.mk to aosp_alphalm.mk

--- aosp_alphalm.mk
+++ aosp_alphalm.mk
@@ -20,29 +20,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit from flashlmdd device
-$(call inherit-product, device/lge/alphaplus/device.mk)
+# Inherit from alphalm device
+$(call inherit-product, device/lge/alphalm/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifiers
 
-PRODUCT_NAME := aosp_alphaplus
-PRODUCT_DEVICE := alphaplus
+PRODUCT_NAME := aosp_alphalm
+PRODUCT_DEVICE := alphalm
 PRODUCT_BRAND := lge
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RELEASE_NAME := LG G8
-PRODUCT_MODEL := LM-G820N
+PRODUCT_MODEL := LM-G850
 
 PRODUCT_GMS_CLIENTID_BASE := android-lge
 
-TARGET_VENDOR_PRODUCT_NAME := alphaplus_lao_com
-TARGET_VENDOR_DEVICE_NAME := alphaplus
+TARGET_VENDOR_PRODUCT_NAME := alphalm_lao_com
+TARGET_VENDOR_DEVICE_NAME := alphalm
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="alphaplus_lao_com" \
-    PRIVATE_BUILD_DESC="alphaplus_lao_com-user 11 RKQ1.210420.001 2122112261c44 release-keys"
+    PRODUCT_NAME="alphalm_lao_com" \
+    PRIVATE_BUILD_DESC="alphalm_lao_com-user 11 RKQ1.210420.001 2122112261c44 release-keys"
 
-BUILD_FINGERPRINT := "lge/alphaplus_lao_com/alphaplus:11/RKQ1.210420.001/2122112261c44:user/release-keys"
+BUILD_FINGERPRINT := "lge/alphalm_lao_com/alphalm:11/RKQ1.210420.001/2122112261c44:user/release-keys"
 
